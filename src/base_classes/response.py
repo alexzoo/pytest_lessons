d997e53655f34baa2d5da897b0b4700dc0e737ee
azubov <zubov.alexey@gmail.com>
2022-10-27 19:44:00 +0300

    add test

---
+++ src/base_classes/response.py
@@ -0,0 +1,41 @@
+from jsonschema import validate
+
+
+class Response:
+
+    """
+    Полезный класс, который помогает нам экономить тонны кода в процессе
+    валидации данных. На вход он принимает объект респонса и разбирает его.
+    Вы можете добавить кучу различных методов в этом классе, которые нужны
+    вам в работе с данными после их получения.
+    It's useful class that helps to save a lot of code during validatio
+    process in our tests. It receives response object and gets from it all
+    values that should be validated. You can add additional methods into the
+    Class if it needs for your project testing.
+    """
+
+    def __init__(self, response):
+        self.response = response
+        self.response_json = response.json()
+        self.response_status = response.status_code
+
+    def validate(self, schema):
+        if isinstance(self.response_json, list):
+            for item in self.response_json:
+                validate(item, schema)
+        else:
+            validate(self.response_json, schema)
+
+    def assert_status_code(self, status_code):
+        """
+        Метод для валидации статус кода. Из объекта респонса,
+        который мы получили, мы берём статус и сравнимаем с тем, который
+        нам был передан как параметр.
+        Method for status code validation. It compares value from response
+        object and compare it with received value from method params.
+        """
+        if isinstance(status_code, list):
+            assert self.response_status in status_code
+        else:
+            assert self.response_status == status_code
+        return self
---
+++ src/enums/global_enums.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class GlobalErrors(Enum):
+    """
+    Обычный класс с ошибками которые стоит использовать в ваших Assert и в
+    Response класс. Нужно это для того, чтобы:
+    1. Унифицировать всё это дело
+    2. Облегчить использование и обновление ошибок
+    Default ENUM class with error that should be used in your asserts and in
+    Response class. It needs for:
+    1. Unification
+    2. Helps in code support
+    """
+    WRONG_STATUS_CODE = 'Status code is different than expected'
