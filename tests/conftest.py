0d8d31d38c7ad4538c17812bfeb78ce728d3e428
azubov <zubov.alexey@gmail.com>
2022-10-31 13:56:32 +0300

    add builders

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,14 @@ import pytest
 import requests
 
 from configuration import SERVICE_URL
+from src.generators.player import Player
 
 
 @pytest.fixture
 def get_users():
     return requests.get(url=SERVICE_URL)
+
+
+@pytest.fixture
+def get_player_generator():
+    return Player()
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,9 +1,11 @@
 import pytest
 
 from src.base_classes.response import Response
+from src.generators.player_localization import PlayerLocalization
 from src.schemas.user import User
 
 
+@pytest.mark.dev
 def test_getting_posts(get_users):
     Response(get_users).assert_status_code(200).validate(User)
 
@@ -15,3 +17,42 @@ def test_getting_posts(get_users):
 ])
 def test_calculate(first_value, second_value, result):
     assert (first_value + second_value) == result
+
+
+@pytest.mark.parametrize('status', [
+    'ACTIVE',
+    'BANNED',
+    'DELETED',
+    'INACTIVE'
+])
+def test_statuses(status, get_player_generator):
+    print(get_player_generator.set_status(status).build())
+
+
+@pytest.mark.parametrize('balance', [
+    '100',
+    '0',
+    '-100',
+    'trololo'
+])
+def test_balance(balance, get_player_generator):
+    print(get_player_generator.set_balance(balance).build())
+
+
+@pytest.mark.parametrize('delete_key', [
+    'account_status',
+    'balance',
+    'localize',
+    'avatar'
+])
+def test_delete_key(delete_key, get_player_generator):
+    object_to_test = get_player_generator.build()
+    del object_to_test[delete_key]
+    print(object_to_test)
+
+
+def test_inner_generator(get_player_generator):
+    object_to_test = get_player_generator.update_inner_generator(
+        'localize', PlayerLocalization('fr_FR')
+    ).build()
+    print(object_to_test)
